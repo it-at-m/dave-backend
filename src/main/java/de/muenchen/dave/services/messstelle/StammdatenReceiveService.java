45950f885c10121d725d07f7f023327f9fd8b9b0
alexander.kerscher <alexander.kerscher@muenchen.de>
2023-12-05 10:45:35 +0100

    neu generiert

--- src/main/java/de/muenchen/dave/services/messstelle/StammdatenReceiveService.java
+++ src/main/java/de/muenchen/dave/services/messstelle/StammdatenReceiveService.java
@@ -1,27 +1,12 @@
 package de.muenchen.dave.services.messstelle;
 
-import com.sun.mail.imap.IMAPFolder;
-import de.muenchen.dave.exceptions.BrokenInfrastructureException;
-import de.muenchen.dave.exceptions.DataNotFoundException;
 import de.muenchen.dave.geodateneai.gen.api.MessstelleApi;
 import de.muenchen.dave.geodateneai.gen.model.MessstelleDto;
-import de.muenchen.dave.services.ChatMessageService;
-import de.muenchen.dave.services.email.ProcessEmailService;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import javax.mail.Flags;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Store;
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * Die Klasse {@link StammdatenReceiveService} holt alle relevanten Messstellen aus MobidaM
@@ -29,6 +14,7 @@ import java.util.Properties;
  */
 @Slf4j
 @Service
+@AllArgsConstructor
 public class StammdatenReceiveService {
 
     private MessstelleApi messstelleApi;
@@ -38,12 +24,14 @@ public class StammdatenReceiveService {
      * Wie oft gecheckt wird, kann in der application.yml geändert werden.
      * Falls keine E-Mail Adresse oder die dummy-Email-Adresse konfiguriert ist, wird nichts getan.
      */
-    @Scheduled(cron = "${dave.messstellen.update-cron}")
+    @Scheduled(cron = "${dave.messstelle.cron}")
     public void loadMessstellen() {
         // Daten aus MobidaM laden
         final List<MessstelleDto> body = messstelleApi.getMessstellenWithHttpInfo().block().getBody();
         // Daten aus Dave laden
-
+        body.forEach(messstelleDto -> {
+            messstelleDto.getMstId();
+        });
         // Daten aktualisieren
 
         // Daten speichern
