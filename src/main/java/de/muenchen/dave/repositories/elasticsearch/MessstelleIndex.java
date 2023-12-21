f88c8e8d4cd01a042e1f0c647e1b3c0ab8bce9ff
martin.dietrich <martin.dietrich@muenchen.de>
2023-12-21 09:58:24 +0100

    cacheevict added

--- src/main/java/de/muenchen/dave/repositories/elasticsearch/MessstelleIndex.java
+++ src/main/java/de/muenchen/dave/repositories/elasticsearch/MessstelleIndex.java
@@ -1,8 +1,10 @@
 package de.muenchen.dave.repositories.elasticsearch;
 
+import de.muenchen.dave.configuration.CachingConfiguration;
 import de.muenchen.dave.domain.elasticsearch.detektor.Messstelle;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
@@ -10,34 +12,34 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 public interface MessstelleIndex extends ElasticsearchRepository<Messstelle, String> {
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     void deleteAll();
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     void deleteAll(Iterable<? extends Messstelle> var1);
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     void deleteById(String var1);
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     void delete(Messstelle var1);
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     <S extends Messstelle> S save(S var1);
 
-    //    @CacheEvict(value = { CachingConfiguration.SUCHE_ZAEHLSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
-    //            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
-    //            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
+    @CacheEvict(value = { CachingConfiguration.SUCHE_ERHEBUNGSSTELLE, CachingConfiguration.SUCHE_ZAEHLSTELLE_DATENPORTAL,
+            CachingConfiguration.LADE_BELASTUNGSPLAN_DTO, CachingConfiguration.LADE_PROCESSED_ZAEHLDATEN,
+            CachingConfiguration.LADE_ZAEHLDATEN_ZEITREIHE_DTO, CachingConfiguration.READ_ZAEHLSTELLE_DTO }, allEntries = true)
     <S extends Messstelle> Iterable<S> saveAll(Iterable<S> var1);
 
     Optional<Messstelle> findById(String var1);
