fe8398368d137b79021b5001430cab064193edad
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-16 13:17:09 +0100

    attribute hinzugefügt für frontend

--- src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
@@ -1,6 +1,10 @@
 package de.muenchen.dave.domain.dtos.messstelle;
 
+import de.muenchen.dave.domain.elasticsearch.detektor.Messquerschnitt;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -10,6 +14,11 @@ public class ReadMessstelleInfoDTO implements Serializable {
     private String mstId;
     private String stadtbezirk;
     private Integer stadtbezirkNummer;
+    private String standort;
     private Double longitude;
     private Double latitude;
+    private String datumLetztePlausibleMessung;
+    private String realisierungsdatum;
+    private String abbaudatum;
+    private List<ReadMessquerschnittDTO> messquerschnitte;
 }
