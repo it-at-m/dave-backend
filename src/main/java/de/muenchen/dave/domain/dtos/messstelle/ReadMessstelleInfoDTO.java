06cc0d9227a5f37733f0c87b661e2fc219a628a1
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-16 14:56:07 +0100

    typen angepasst

--- src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
@@ -1,6 +1,7 @@
 package de.muenchen.dave.domain.dtos.messstelle;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
 
@@ -14,8 +15,8 @@ public class ReadMessstelleInfoDTO implements Serializable {
     private String standort;
     private Double longitude;
     private Double latitude;
-    private String datumLetztePlausibleMessung;
-    private String realisierungsdatum;
-    private String abbaudatum;
+    private LocalDate datumLetztePlausibleMessung;
+    private LocalDate realisierungsdatum;
+    private LocalDate abbaudatum;
     private List<ReadMessquerschnittDTO> messquerschnitte;
 }
