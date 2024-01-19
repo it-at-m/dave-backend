6d6c16acb2f236c8a2b0b200cf1f33295b28e41e
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-19 13:04:30 +0100

    DTO erweitert für benötigte daten im frontend

--- src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/messstelle/ReadMessstelleInfoDTO.java
@@ -2,14 +2,21 @@ package de.muenchen.dave.domain.dtos.messstelle;
 
 import java.io.Serializable;
 import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class ReadMessstelleInfoDTO implements Serializable {
 
     private String id;
     private String mstId;
+    private String standort;
     private String stadtbezirk;
     private Integer stadtbezirkNummer;
     private Double longitude;
     private Double latitude;
+    private LocalDate datumLetztePlausibleMessung;
+    private LocalDate realisierungsdatum;
+    private LocalDate abbaudatum;
+    private List<ReadMessquerschnittDTO> messquerschnitte;
 }
