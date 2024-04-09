a5c37a8ac76c083b5fcf52b962e6c2bb84ae542b
alexander.kerscher <alexander.kerscher@muenchen.de>
2024-04-09 10:15:39 +0200

    sort Messstellen by mstid

--- src/main/java/de/muenchen/dave/services/messstelle/MessstelleService.java
+++ src/main/java/de/muenchen/dave/services/messstelle/MessstelleService.java
@@ -8,9 +8,13 @@ import de.muenchen.dave.domain.dtos.messstelle.auswertung.MessstelleAuswertungDT
 import de.muenchen.dave.domain.elasticsearch.detektor.Messstelle;
 import de.muenchen.dave.domain.mapper.detektor.MessstelleMapper;
 import de.muenchen.dave.services.CustomSuggestIndexService;
+
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -75,6 +79,7 @@ public class MessstelleService {
     public List<MessstelleAuswertungDTO> getAllVisibleMessstellenForAuswertung() {
         log.debug("#getAllVisibleMessstellenForAuswertung");
         final List<Messstelle> messstellen = messstelleIndexService.findAllVisibleMessstellen();
-        return messstelleMapper.bean2auswertungDto(messstellen);
+        final List<Messstelle> sorted = messstellen.stream().sorted(Comparator.comparing(Messstelle::getMstId)).collect(Collectors.toList());
+        return messstelleMapper.bean2auswertungDto(sorted);
     }
 }
