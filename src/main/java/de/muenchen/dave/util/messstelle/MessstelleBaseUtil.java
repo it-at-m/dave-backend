bee7f6f1554e6d483e7da11d015b480ae70c8abb
alexander.kerscher <alexander.kerscher@muenchen.de>
2024-04-12 14:58:03 +0200

    fix belastungsplan

--- src/main/java/de/muenchen/dave/util/messstelle/MessstelleBaseUtil.java
+++ src/main/java/de/muenchen/dave/util/messstelle/MessstelleBaseUtil.java
@@ -7,32 +7,35 @@ package de.muenchen.dave.util.messstelle;
 import de.muenchen.dave.geodateneai.gen.model.ChosenTagesTypValidRequestDto;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MessstelleBaseUtil {
 
     public static String getTextOfTagesTyp(final String tagestyp) {
-        ChosenTagesTypValidRequestDto.TagesTypEnum tagesTypEnum = ChosenTagesTypValidRequestDto.TagesTypEnum.fromValue(tagestyp);
         String text = tagestyp;
-        switch (tagesTypEnum) {
-        case WERKTAG_DI_MI_DO:
-            text = "DTVw3 (Di,Mi,Do - außerhalb Ferien)";
-            break;
-        case WERKTAG_MO_FR:
-            text = "DTVw5 (Mo-Fr - außerhalb Ferien)";
-            break;
-        case SAMSTAG:
-            text = "Samstag in/außerhalb Ferien";
-            break;
-        case SONNTAG_FEIERTAG:
-            text = "Sonntag/Feiertag in/außerhalb Ferien";
-            break;
-        case WERKTAG_FERIEN:
-            text = "Mo-Fr Ferien";
-            break;
-        case MO_SO:
-            text = "DTV (MO - SO)";
-            break;
+        if (StringUtils.isNotEmpty(tagestyp)) {
+            ChosenTagesTypValidRequestDto.TagesTypEnum tagesTypEnum = ChosenTagesTypValidRequestDto.TagesTypEnum.fromValue(tagestyp);
+            switch (tagesTypEnum) {
+            case WERKTAG_DI_MI_DO:
+                text = "DTVw3 (Di,Mi,Do - außerhalb Ferien)";
+                break;
+            case WERKTAG_MO_FR:
+                text = "DTVw5 (Mo-Fr - außerhalb Ferien)";
+                break;
+            case SAMSTAG:
+                text = "Samstag in/außerhalb Ferien";
+                break;
+            case SONNTAG_FEIERTAG:
+                text = "Sonntag/Feiertag in/außerhalb Ferien";
+                break;
+            case WERKTAG_FERIEN:
+                text = "Mo-Fr Ferien";
+                break;
+            case MO_SO:
+                text = "DTV (MO - SO)";
+                break;
+            }
         }
         return text;
     }
