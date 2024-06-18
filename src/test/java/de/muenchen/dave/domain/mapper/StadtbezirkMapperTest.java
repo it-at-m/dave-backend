2e0753152c397ca944e513073029d81f05b0dc73
martin.dietrich <martin.dietrich@muenchen.de>
2024-06-18 13:08:03 +0200

    tests fixed

--- src/test/java/de/muenchen/dave/domain/mapper/StadtbezirkMapperTest.java
+++ src/test/java/de/muenchen/dave/domain/mapper/StadtbezirkMapperTest.java
@@ -5,11 +5,15 @@ import static de.muenchen.dave.TestConstants.SPRING_TEST_PROFILE;
 
 import com.github.javafaker.Faker;
 import de.muenchen.dave.DaveBackendApplication;
+import de.muenchen.dave.repositories.elasticsearch.CustomSuggestIndex;
+import de.muenchen.dave.repositories.elasticsearch.MessstelleIndex;
+import de.muenchen.dave.repositories.elasticsearch.ZaehlstelleIndex;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = { DaveBackendApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
@@ -21,6 +25,15 @@ public class StadtbezirkMapperTest {
 
     @Autowired
     private StadtbezirkMapper stadtbezirkMapper;
+
+    @MockBean
+    private ZaehlstelleIndex zaehlstelleIndex;
+
+    @MockBean
+    private MessstelleIndex messstelleIndex;
+
+    @MockBean
+    private CustomSuggestIndex customSuggestIndex;
 
     private static final Faker fakerInstance = Faker.instance();
 
