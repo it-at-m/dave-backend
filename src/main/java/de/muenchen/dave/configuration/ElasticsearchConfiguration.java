8851fd080faa50548160ec6797c98d255409d0bd
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-07-09 10:22:23 +0200

    adapt elasticsearch config

--- src/main/java/de/muenchen/dave/configuration/ElasticsearchConfiguration.java
+++ src/main/java/de/muenchen/dave/configuration/ElasticsearchConfiguration.java
@@ -1,15 +1,13 @@
 package de.muenchen.dave.configuration;
 
 import java.time.Duration;
+
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.client.RestHighLevelClient;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
@@ -36,20 +34,13 @@ public class ElasticsearchConfiguration {
     private int socketTimeout;
 
     @Bean
-    public RestHighLevelClient client() {
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+    public ClientConfiguration client() {
+        return ClientConfiguration.builder()
                 .connectedTo(this.host + ":" + this.port)
                 .withBasicAuth(this.user, this.password)
                 .withConnectTimeout(Duration.ofSeconds(connectTimeout))
                 .withSocketTimeout(Duration.ofSeconds(socketTimeout))
                 .build();
-
-        return RestClients.create(clientConfiguration).rest();
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate(RestHighLevelClient client) {
-        return new ElasticsearchRestTemplate(client);
     }
 
 }
