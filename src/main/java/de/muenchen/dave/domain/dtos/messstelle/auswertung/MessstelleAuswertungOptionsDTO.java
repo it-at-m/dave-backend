2bdb485908817547488f1130b44e80c2972600b3
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-11-11 10:45:22 +0100

    add todo

--- src/main/java/de/muenchen/dave/domain/dtos/messstelle/auswertung/MessstelleAuswertungOptionsDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/messstelle/auswertung/MessstelleAuswertungOptionsDTO.java
@@ -15,6 +15,8 @@ public class MessstelleAuswertungOptionsDTO implements Serializable {
     private List<Integer> jahre;
     private TagesTyp tagesTyp;
     private List<AuswertungsZeitraum> zeitraum;
+
+    // TODO: Zusammenfassen der mstIds und mqids in eine Liste von Objekten für Mst welche jeweils die mqId beinhalten.
     private Set<String> mstIds;
     private Set<String> mqIds;
     private FahrzeugOptionsDTO fahrzeuge;
