932b3195ff51d6e33a93ef51d43e0f6e127123c2
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-12-03 15:37:29 +0100

    remove unused imports

--- src/main/java/de/muenchen/dave/controller/AuswertungController.java
+++ src/main/java/de/muenchen/dave/controller/AuswertungController.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
