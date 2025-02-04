103bfaa27462d6f3a7470a45cea2eb9c5f9f85b5
boal <56300801+boal@users.noreply.github.com>
2025-02-04 13:27:54 +0100

    Bugfixes (#203)
    * fix DAVE-454 Wochentagbezeichnung
    
    * fix DAVE-441 Listenausgabe als csv
    
    * fix warning
    
    * fix DAVE-443
    
    * fix test
    
    * hebung spring framework
    
    * fix missing boolean values
    
    * fix DAVE-457
    
    ---------
    
    Co-authored-by: alexander.kerscher <alexander.kerscher@muenchen.de>

--- src/main/java/de/muenchen/dave/domain/enums/ZaehldatenIntervall.java
+++ src/main/java/de/muenchen/dave/domain/enums/ZaehldatenIntervall.java
@@ -12,13 +12,13 @@ import lombok.Getter;
 @Getter
 public enum ZaehldatenIntervall {
 
-    STUNDE_VIERTEL(1, 15, MesswertRequestDto.IntervalInMinutesEnum._15, TypeZeitintervall.STUNDE_VIERTEL),
+    STUNDE_VIERTEL(1, 15, MesswertRequestDto.IntervalInMinutesEnum.INTERVAL_15, TypeZeitintervall.STUNDE_VIERTEL),
 
-    STUNDE_VIERTEL_EINGESCHRAENKT(1, 15, MesswertRequestDto.IntervalInMinutesEnum._15, TypeZeitintervall.STUNDE_VIERTEL),
+    STUNDE_VIERTEL_EINGESCHRAENKT(1, 15, MesswertRequestDto.IntervalInMinutesEnum.INTERVAL_15, TypeZeitintervall.STUNDE_VIERTEL),
 
-    STUNDE_HALB(2, 30, MesswertRequestDto.IntervalInMinutesEnum._30, TypeZeitintervall.STUNDE_HALB),
+    STUNDE_HALB(2, 30, MesswertRequestDto.IntervalInMinutesEnum.INTERVAL_30, TypeZeitintervall.STUNDE_HALB),
 
-    STUNDE_KOMPLETT(4, 60, MesswertRequestDto.IntervalInMinutesEnum._60, TypeZeitintervall.STUNDE_KOMPLETT);
+    STUNDE_KOMPLETT(4, 60, MesswertRequestDto.IntervalInMinutesEnum.INTERVAL_60, TypeZeitintervall.STUNDE_KOMPLETT);
 
     private final Integer quarterPerIntervall;
 
