897bd16695a14e35b650aed10ea7f834d4fea340
martin.dietrich <martin.dietrich@muenchen.de>
2025-02-10 09:02:16 +0100

    review comments

--- src/main/java/de/muenchen/dave/repositories/relationaldb/KalendertagRepository.java
+++ src/main/java/de/muenchen/dave/repositories/relationaldb/KalendertagRepository.java
@@ -7,12 +7,8 @@ package de.muenchen.dave.repositories.relationaldb;
 import de.muenchen.dave.domain.Kalendertag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface KalendertagRepository extends JpaRepository<Kalendertag, UUID> { //NOSONAR
-
-    @Override
-    Optional<Kalendertag> findById(UUID id);
 
 }
