60efa5a0aae8afcc353663f1ee62f568d9eb7780
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-02-17 14:28:05 +0100

    add entity for unuaffaellige tage

---
+++ src/main/java/de/muenchen/dave/domain/UnauffaelligeTage.java
@@ -0,0 +1,38 @@
+package de.muenchen.dave.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(
+        indexes = {
+                @Index(
+                        name = "index_unauffaellige_tage_mst_id_datum",
+                        columnList = "mst_id, datum"
+                )
+        },
+        uniqueConstraints = { @UniqueConstraint(
+                name = "unique_unauffaellige_tage_mst_id_datum",
+                columnNames = { "mst_id", "datum" }
+        )
+        }
+)
+public class UnauffaelligeTage extends BaseEntity {
+
+    @Column(nullable = false)
+    private Integer mstId;
+
+    @Column(nullable = false)
+    private LocalDate datum;
+}
