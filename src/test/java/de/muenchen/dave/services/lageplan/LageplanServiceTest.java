b443678d47c7f2533dc89b634c92ffa7db3261bd
martin.dietrich <martin.dietrich@muenchen.de>
2025-02-24 14:02:18 +0100

    tests added

--- src/test/java/de/muenchen/dave/services/lageplan/LageplanServiceTest.java
+++ src/test/java/de/muenchen/dave/services/lageplan/LageplanServiceTest.java
@@ -1,6 +1,7 @@
 package de.muenchen.dave.services.lageplan;
 
 import de.muenchen.dave.documentstorage.gen.api.LageplanApi;
+import de.muenchen.dave.exceptions.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 @Slf4j
@@ -40,11 +44,24 @@ class LageplanServiceTest {
                 .isEqualTo(true);
     }
 
-    //    @Test // TODO
-    //    void testLageplanVorhanden_WithNichtVorhanden() {
-    //    }
-    //
-    //    @Test // TODO
-    //    void testLageplanVorhanden_WithException() {
-    //    }
+        @Test
+        void testLageplanVorhanden_WithNichtVorhanden() {
+
+            String mstId = "9999";
+            Mockito.when(lageplanApi.lageplanExistsWithHttpInfo(mstId)).thenReturn(Mono.just(ResponseEntity.of(Optional.of(false))));
+            Boolean result = lageplanService.lageplanVorhanden(mstId);
+
+            Assertions.assertThat(result)
+                    .isNotNull()
+                    .isEqualTo(false);
+        }
+
+        @Test
+        void testLageplanVorhanden_WithException() {
+
+            String mstId = "9999";
+            Mockito.when(lageplanApi.lageplanExistsWithHttpInfo(mstId)).thenReturn(Mono.just(new ResponseEntity<>(null, HttpStatus.OK)));
+
+            assertThrows(ResourceNotFoundException.class, () -> lageplanService.lageplanVorhanden(mstId));
+        }
 }
