e748167a5f3224b9da0849f7882bae53e772cbcc
martind260 <99957516+martind260@users.noreply.github.com>
2025-02-27 11:45:03 +0100

    Db schemas (#230)
    * change db-script and entity
    
    * ferien rem. from kalendertag, insert added
    
    * set db schema dynamically
    
    * flyway plugin added
    
    * env-spec indices impl.
    
    * test properties fixed
    
    * review comments incorporated
    
    * try-res-cascading rem.
    
    * final adjusted
    
    ---------
    
    Co-authored-by: alexander.kerscher <alexander.kerscher@muenchen.de>

---
+++ src/main/java/de/muenchen/dave/configuration/FlywaySchemaCallback.java
@@ -0,0 +1,55 @@
+package de.muenchen.dave.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.api.callback.Callback;
+import org.flywaydb.core.api.callback.Context;
+import org.flywaydb.core.api.callback.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Die Klasse implementiert die Flyway Callback-Schnittstelle, um vor bestimmten Migrationsschritten
+ * zusätzl. Vorarbeiten durchzuführen.
+ */
+@Component
+@Slf4j
+public class FlywaySchemaCallback implements Callback {
+
+    private final String schema;
+
+    public FlywaySchemaCallback(@Value("${db.schema}") final String schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public boolean supports(final Event event, final Context context) {
+        // telling Flyway to only trigger callback for these events
+        return event.equals(Event.BEFORE_EACH_MIGRATE) || event.equals(Event.AFTER_EACH_MIGRATE);
+    }
+
+    @Override
+    public boolean canHandleInTransaction(final Event event, final Context context) {
+        return false;
+    }
+
+    @Override
+    public void handle(final Event event, final Context context) {
+        if (event.equals(org.flywaydb.core.api.callback.Event.BEFORE_MIGRATE)) {
+            try (final Connection connection = context.getConnection();
+                    final Statement statement = connection.createStatement()) {
+                statement.execute("SET search_path TO " + schema);
+            } catch (SQLException e) {
+                log.error("Fehler bei der Migrationsvorbereitung", e);
+            }
+        }
+    }
+
+    @Override
+    public String getCallbackName() {
+        return this.getClass().getSimpleName();
+    }
+}
