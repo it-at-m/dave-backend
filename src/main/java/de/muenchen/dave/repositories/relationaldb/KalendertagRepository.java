b4f445384a386f00350d986bea66cea9f8de57b2
Der-Alex-K <18263366+Der-Alex-K@users.noreply.github.com>
2025-03-03 11:50:34 +0100

    Dave 449 kalendertabelle (#220)

---
+++ src/main/java/de/muenchen/dave/repositories/relationaldb/KalendertagRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2021
+ */
+package de.muenchen.dave.repositories.relationaldb;
+
+import de.muenchen.dave.domain.Kalendertag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface KalendertagRepository extends JpaRepository<Kalendertag, UUID> { //NOSONAR
+
+}
