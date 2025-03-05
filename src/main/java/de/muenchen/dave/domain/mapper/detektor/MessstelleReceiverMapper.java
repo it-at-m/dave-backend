47ebeb6c5784bcb87315abe3f9c6c3ac35e9689e
boal <56300801+boal@users.noreply.github.com>
2025-03-05 12:41:33 +0100

    DAVE-294 Synchronisierung unauffällige Tage (#229)

--- src/main/java/de/muenchen/dave/domain/mapper/detektor/MessstelleReceiverMapper.java
+++ src/main/java/de/muenchen/dave/domain/mapper/detektor/MessstelleReceiverMapper.java
@@ -1,5 +1,7 @@
 package de.muenchen.dave.domain.mapper.detektor;
 
+import de.muenchen.dave.domain.Kalendertag;
+import de.muenchen.dave.domain.UnauffaelligerTag;
 import de.muenchen.dave.domain.elasticsearch.detektor.Messfaehigkeit;
 import de.muenchen.dave.domain.elasticsearch.detektor.Messquerschnitt;
 import de.muenchen.dave.domain.elasticsearch.detektor.Messstelle;
@@ -7,6 +9,7 @@ import de.muenchen.dave.domain.mapper.StadtbezirkMapper;
 import de.muenchen.dave.geodateneai.gen.model.MessfaehigkeitDto;
 import de.muenchen.dave.geodateneai.gen.model.MessquerschnittDto;
 import de.muenchen.dave.geodateneai.gen.model.MessstelleDto;
+import de.muenchen.dave.geodateneai.gen.model.UnauffaelligerTagDto;
 import de.muenchen.dave.util.SuchwortUtil;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
@@ -96,5 +99,13 @@ public interface MessstelleReceiverMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "standort", ignore = true)
     Messquerschnitt updateMessquerschnitt(@MappingTarget Messquerschnitt existing, MessquerschnittDto dto, @Context StadtbezirkMapper stadtbezirkMapper);
+
+    @Mapping(target = "kalendertag", ignore = true)
+    UnauffaelligerTag dto2Entity(final UnauffaelligerTagDto dto, @Context final Kalendertag kalendertag);
+
+    @AfterMapping
+    default void dto2EntityAfterMapping(@MappingTarget final UnauffaelligerTag entity, @Context final Kalendertag kalendertag) {
+        entity.setKalendertag(kalendertag);
+    }
 
 }
--- src/main/java/de/muenchen/dave/repositories/relationaldb/KalendertagRepository.java
+++ src/main/java/de/muenchen/dave/repositories/relationaldb/KalendertagRepository.java
@@ -1,14 +1,13 @@
-/*
- * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
- * der Landeshauptstadt München, 2021
- */
 package de.muenchen.dave.repositories.relationaldb;
 
 import de.muenchen.dave.domain.Kalendertag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface KalendertagRepository extends JpaRepository<Kalendertag, UUID> { //NOSONAR
 
+    Optional<Kalendertag> findByDatum(final LocalDate datum);
 }
