6ef920245519192c4cd9324b3b4ed4eef0bfe602
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-03-17 07:23:24 +0100

    fix possible Nullpointer

--- src/main/java/de/muenchen/dave/services/messstelle/MesswerteService.java
+++ src/main/java/de/muenchen/dave/services/messstelle/MesswerteService.java
@@ -67,7 +67,7 @@ public class MesswerteService {
 
         final var meanPerMessquerschnitt = ListUtils.emptyIfNull(response.getMeanOfIntervalsForEachMqIdByMesstag())
                 .stream()
-                .flatMap(intervalsForMqId -> CollectionUtils.emptyIfNull(intervalsForMqId.getMeanOfIntervalsByMesstag()).stream())
+                .flatMap(intervalsForMqId -> ListUtils.emptyIfNull(intervalsForMqId.getMeanOfIntervalsByMesstag()).stream())
                 .toList();
 
         final var processedZaehldaten = new LadeProcessedMesswerteDTO();
