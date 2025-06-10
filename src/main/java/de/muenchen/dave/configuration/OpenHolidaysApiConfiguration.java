255a5f2ab5f7eee40996228dfb3a7a337320eb3a
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-06-10 07:40:05 +0200

    add openholidaysapi as prototype

---
+++ src/main/java/de/muenchen/dave/configuration/OpenHolidaysApiConfiguration.java
@@ -0,0 +1,45 @@
+package de.muenchen.dave.configuration;
+
+import de.muenchen.dave.openholidays.gen.api.HolidaysApi;
+import de.muenchen.dave.openholidays.gen.geodaten.ApiClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
+
+@Configuration
+@RequiredArgsConstructor
+public class OpenHolidaysApiConfiguration {
+
+    @Value("${dave.holidays.url}")
+    public String openholidaysUrl;
+
+    @Value("${internet.proxy}")
+    public String proxyUrl;
+
+    /**
+     * Erstellt eine unsecured {@link HolidaysApi} Bean für Requests an openholidaysapi.org.
+     */
+    @Bean
+    public HolidaysApi holidaysApi() {
+        final WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient()))
+                .build();
+        final ApiClient apiClient = this.documentStorageApiClient(webClient);
+        return new HolidaysApi(apiClient);
+    }
+
+    private ApiClient documentStorageApiClient(final WebClient webClient) {
+        final var apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(openholidaysUrl);
+        return apiClient;
+    }
+
+    private HttpClient httpClient() {
+        return HttpClient.create().proxy(typeSpec -> typeSpec.type(ProxyProvider.Proxy.HTTP).host(proxyUrl).port(80).build());
+    }
+}
