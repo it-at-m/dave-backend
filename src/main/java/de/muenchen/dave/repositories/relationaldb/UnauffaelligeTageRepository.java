7b7acfbea5f4305b0bd880050b96c90f8d9a5c3d
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-10-30 11:26:34 +0100

    add missing cacheevicts

--- src/main/java/de/muenchen/dave/repositories/relationaldb/UnauffaelligeTageRepository.java
+++ src/main/java/de/muenchen/dave/repositories/relationaldb/UnauffaelligeTageRepository.java
@@ -40,6 +40,12 @@ public interface UnauffaelligeTageRepository extends JpaRepository<Unauffaellige
             value = { CachingConfiguration.AUFFAELLIGETAGE_FOR_MESSSTELLE },
             allEntries = true
     )
+    void deleteAllByKalendertagDatum(final LocalDate kalendertagDatum);
+
+    @CacheEvict(
+            value = { CachingConfiguration.AUFFAELLIGETAGE_FOR_MESSSTELLE },
+            allEntries = true
+    )
     <S extends UnauffaelligerTag> S save(S var1);
 
     @CacheEvict(
@@ -47,6 +53,12 @@ public interface UnauffaelligeTageRepository extends JpaRepository<Unauffaellige
             allEntries = true
     )
     <S extends UnauffaelligerTag> List<S> saveAll(Iterable<S> var1);
+
+    @CacheEvict(
+            value = { CachingConfiguration.AUFFAELLIGETAGE_FOR_MESSSTELLE },
+            allEntries = true
+    )
+    <S extends UnauffaelligerTag> List<S> saveAllAndFlush(Iterable<S> entities);
 
     List<UnauffaelligerTag> findByMstId(final String mstId);
 
@@ -57,6 +69,4 @@ public interface UnauffaelligeTageRepository extends JpaRepository<Unauffaellige
             final LocalDate startDateIncluded,
             final LocalDate endDateIncluded,
             final List<TagesTyp> tagesTyp);
-
-    void deleteAllByKalendertagDatum(final LocalDate kalendertagDatum);
 }
