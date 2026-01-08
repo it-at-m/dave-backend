de8cf2a608a6a0837d4c875e82d9a38111c7e0e4
Anett Hübner <anett_huebner@gmx.de>
2026-01-08 18:57:40 +0100

    added zaehlstelle for relative db

--- src/main/java/de/muenchen/dave/domain/analytics/Zaehlstelle.java
+++ src/main/java/de/muenchen/dave/domain/analytics/Zaehlstelle.java
@@ -1,8 +1,16 @@
 package de.muenchen.dave.domain.analytics;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.elasticsearch.annotations.GeoPointField;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
 import de.muenchen.dave.domain.BaseEntity;
+import de.muenchen.dave.domain.elasticsearch.Zaehlung;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +35,7 @@ public class Zaehlstelle extends BaseEntity {
     @Column(name = "stadtbezirk_nummer")
     Integer stadtbezirkNummer;
 
+    @Column(name = "kommentar")
     String kommentar;
 
     @Column(name = "letzte_zaehlung_monat_nummer")
@@ -46,4 +55,10 @@ public class Zaehlstelle extends BaseEntity {
      */
     @Column(name = "sichtbar_datenportal")
     Boolean sichtbarDatenportal;
+
+    @Transient
+    GeoPoint punkt;
+
+    @Transient
+    List<Zaehlung> zaehlungen = new ArrayList<>();
 }
