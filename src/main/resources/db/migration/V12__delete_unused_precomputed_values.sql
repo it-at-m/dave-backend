f858fa9cf5e843ce2b35bdfc43c9900a8e52f81c
martin.dietrich <martin.dietrich@muenchen.de>
2026-02-12 14:58:17 +0100

    Fahrbez. renamed

--- src/main/resources/db/migration/V12__delete_unused_precomputed_values.sql
+++ src/main/resources/db/migration/V12__delete_unused_precomputed_values.sql
@@ -3,4 +3,4 @@ DELETE FROM zeitintervall WHERE TYPE = 'SPITZENSTUNDE_KFZ' OR TYPE = 'SPITZENSTU
 
 -- Alle Datensätze von Zeitintervall, wo Zeitintervall.Fahrbeziehung.von=NULL oder Zeitintervall.Fahrbeziehung.nach=NULL (kein Kreisverkehr) löschen
 DELETE FROM zeitintervall
-WHERE (fahrbeziehung_von IS NULL OR fahrbeziehung_nach IS NULL) AND fahrbeziehung_fahrbewegungkreisverkehr IS NULL;
+WHERE (verkehrsbeziehung_von IS NULL OR verkehrsbeziehung_nach IS NULL) AND verkehrsbeziehung_fahrbewegungkreisverkehr IS NULL;
