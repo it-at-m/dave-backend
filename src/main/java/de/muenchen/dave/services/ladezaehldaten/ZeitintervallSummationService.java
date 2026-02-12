142055342bdb0c5d7cbeb8220f3d5682c68259e7
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2026-02-12 17:25:02 +0100

    add null sage summation

--- src/main/java/de/muenchen/dave/services/ladezaehldaten/ZeitintervallSummationService.java
+++ src/main/java/de/muenchen/dave/services/ladezaehldaten/ZeitintervallSummationService.java
@@ -1,11 +1,14 @@
 package de.muenchen.dave.services.ladezaehldaten;
 
 import de.muenchen.dave.domain.Bewegungsbeziehung;
+import de.muenchen.dave.domain.Hochrechnung;
 import de.muenchen.dave.domain.Zeitintervall;
 import de.muenchen.dave.util.dataimport.ZeitintervallBaseUtil;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,10 +19,11 @@ public class ZeitintervallSummationService {
         //Die Map wird invertiert: neuer Schlüssl: Intervalle
         // Die Datenstruktur wird zur Berechnung der SPitzenstunde gebraucht
         //Die Info welche Bewegungsbeziehung es angehört geht verlohren
-        List<Zeitintervall> concatenatedZeitintervall = new ArrayList<>();
-        for (Map.Entry<Bewegungsbeziehung, List<Zeitintervall>> entry : zeitintervalleByBewegungsbeziehung.entrySet()) {
-            concatenatedZeitintervall.addAll(entry.getValue());
-        }
+        List<Zeitintervall> concatenatedZeitintervall = zeitintervalleByBewegungsbeziehung
+                .values()
+                .stream()
+                .flatMap(zeitintervalleOfBewegungsbeziehung -> zeitintervalleOfBewegungsbeziehung.stream())
+                .toList();
         final Map<ZeitintervallBaseUtil.Intervall, List<Zeitintervall>> zeitintervalleGroupedByIntervall = ZeitintervallBaseUtil
                 .createByIntervallGroupedZeitintervalle(concatenatedZeitintervall);
 
@@ -30,12 +34,22 @@ public class ZeitintervallSummationService {
         for (Map.Entry<ZeitintervallBaseUtil.Intervall, List<Zeitintervall>> entry : zeitintervalleGroupedByIntervall.entrySet()) {
             Zeitintervall addedZeitintervall = new Zeitintervall();
             for (Zeitintervall zeitintervall : entry.getValue()) {
-                addedZeitintervall = ZeitintervallBaseUtil.summation(addedZeitintervall, zeitintervall);
+                addedZeitintervall = nullSafeSummationForHochrechnung(addedZeitintervall, zeitintervall);
             }
             summedZeitintervalls.add(addedZeitintervall);
         }
 
         return summedZeitintervalls;
+    }
+
+    protected Zeitintervall nullSafeSummationForHochrechnung(final Zeitintervall zeitintervall1, final Zeitintervall zeitintervall2) {
+        if (Objects.isNull(zeitintervall1.getHochrechnung())) {
+            zeitintervall1.setHochrechnung(new Hochrechnung());
+        }
+        if (Objects.isNull(zeitintervall2.getHochrechnung())) {
+            zeitintervall2.setHochrechnung(new Hochrechnung());
+        }
+        return ZeitintervallBaseUtil.summation(zeitintervall1, zeitintervall2);
     }
 
 }
