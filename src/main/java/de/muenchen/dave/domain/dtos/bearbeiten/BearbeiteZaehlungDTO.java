cc9e0b3d6b8fc1fd79051477d10c41971e72d6f6
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-04-16 09:49:33 +0200

    remove BearbeiteZaehlung validation

--- src/main/java/de/muenchen/dave/domain/dtos/bearbeiten/BearbeiteZaehlungDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/bearbeiten/BearbeiteZaehlungDTO.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import de.muenchen.dave.configuration.json.EmptyListSerializer;
 import de.muenchen.dave.domain.dtos.PkwEinheitDTO;
 import de.muenchen.dave.domain.enums.Fahrzeug;
-import de.muenchen.dave.validation.BearbeiteZaehlungValid;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 
 @Data
-@BearbeiteZaehlungValid
 public class BearbeiteZaehlungDTO {
 
     String id;
