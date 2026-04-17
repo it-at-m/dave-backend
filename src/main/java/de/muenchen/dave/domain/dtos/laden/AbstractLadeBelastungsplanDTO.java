c6b12a263463c0b53c4e392d8d24aa39891b7da2
martin.dietrich <martin.dietrich@muenchen.de>
2026-04-17 08:52:35 +0200

    discriminator added

--- src/main/java/de/muenchen/dave/domain/dtos/laden/AbstractLadeBelastungsplanDTO.java
+++ src/main/java/de/muenchen/dave/domain/dtos/laden/AbstractLadeBelastungsplanDTO.java
@@ -1,9 +1,17 @@
 package de.muenchen.dave.domain.dtos.laden;
 
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 
 @Data
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "belastungsplanTyp")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = LadeBelastungsplanDTO.class, name = "DEFAULT"),
+        @JsonSubTypes.Type(value = LadeBelastungsplanQJSDTO.class, name = "QJS")
+})
 public abstract class AbstractLadeBelastungsplanDTO<T> implements Serializable {
 
     protected String[] streets;
