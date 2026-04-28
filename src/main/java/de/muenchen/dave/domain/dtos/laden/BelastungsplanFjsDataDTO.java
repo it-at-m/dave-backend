af512835433321b87a58b266e115160d20c575d8
martin.dietrich <martin.dietrich@muenchen.de>
2026-04-28 12:45:06 +0200

    first draft

---
+++ src/main/java/de/muenchen/dave/domain/dtos/laden/BelastungsplanFjsDataDTO.java
@@ -0,0 +1,34 @@
+package de.muenchen.dave.domain.dtos.laden;
+
+import de.muenchen.dave.domain.enums.Himmelsrichtung;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BelastungsplanFjsDataDTO extends AbstractBelastungsplanDataDTO {
+
+    private BigDecimal sumAll;
+    private List<StrassenseiteValue> valuesStrassenseite;
+    private List<LaengsverkehrValue> valuesLaengsverkehr;
+
+    @Data
+    @RequiredArgsConstructor
+    public static class LaengsverkehrValue implements Serializable {
+        private final int von;
+        private final Himmelsrichtung strassenseite;
+        private final BigDecimal value;
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    public static class StrassenseiteValue implements Serializable {
+        private final Himmelsrichtung strassenseite;
+        private BigDecimal value;
+    }
+}
