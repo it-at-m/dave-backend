a60ab80f7a0d376caacf0e7ad9fef81ae0bfc9f2
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-04-30 15:41:28 +0200

    validate that at least one base layer is configured

--- src/main/java/de/muenchen/dave/properties/MapConfigProperties.java
+++ src/main/java/de/muenchen/dave/properties/MapConfigProperties.java
@@ -1,20 +1,26 @@
 package de.muenchen.dave.properties;
 
 import de.muenchen.dave.domain.dtos.init.LayerDTO;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties(prefix = "dave.map")
+@Validated
 @Getter
 @Setter
 public class MapConfigProperties {
     private String centerLat = "48.137227";
     private String centerLng = "11.575517";
     private Integer centerZoom = 12;
+
+    @NotEmpty(message = "Es muss mindestens ein Basis-Layer konfiguriert sein (dave.map.baseLayers).")
     private List<LayerDTO> baseLayers;
+
     private List<LayerDTO> overlayLayers;
 }
