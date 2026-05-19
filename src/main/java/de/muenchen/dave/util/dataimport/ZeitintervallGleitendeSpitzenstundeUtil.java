194bd7dd1889371f888087f567fa1673e7e9216d
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2026-05-19 16:57:24 +0200

    adapt method gleitende spitzenstunde

--- src/main/java/de/muenchen/dave/util/dataimport/ZeitintervallGleitendeSpitzenstundeUtil.java
+++ src/main/java/de/muenchen/dave/util/dataimport/ZeitintervallGleitendeSpitzenstundeUtil.java
@@ -108,24 +108,28 @@ public final class ZeitintervallGleitendeSpitzenstundeUtil {
         List<Zeitintervall> gleitendeSpitzenstunden = new ArrayList<>();
         if (Objects.nonNull(zaehlungId)) {
             List<Zeitintervall> calculatedSpitzenstunden;
-            if (Zeitblock.ZB_00_06.equals(zeitblock) || Zeitblock.ZB_00_24.equals(zeitblock)) {
+            if (isZeitblockToCompareWithinGivenZeitblock(Zeitblock.ZB_00_06, zeitblock)) {
                 calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_00_06, zeitintervalle, types);
                 gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
             }
-            if (Zeitblock.ZB_06_10.equals(zeitblock) || Zeitblock.ZB_00_24.equals(zeitblock)) {
+            if (isZeitblockToCompareWithinGivenZeitblock(Zeitblock.ZB_06_10, zeitblock)) {
                 calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_06_10, zeitintervalle, types);
                 gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
             }
-            if (Zeitblock.ZB_10_15.equals(zeitblock) || Zeitblock.ZB_00_24.equals(zeitblock)) {
+            if (isZeitblockToCompareWithinGivenZeitblock(Zeitblock.ZB_10_15, zeitblock)) {
                 calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_10_15, zeitintervalle, types);
                 gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
             }
-            if (Zeitblock.ZB_15_19.equals(zeitblock) || Zeitblock.ZB_00_24.equals(zeitblock)) {
+            if (isZeitblockToCompareWithinGivenZeitblock(Zeitblock.ZB_15_19, zeitblock)) {
                 calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_15_19, zeitintervalle, types);
                 gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
             }
-            if (Zeitblock.ZB_19_24.equals(zeitblock) || Zeitblock.ZB_00_24.equals(zeitblock)) {
+            if (isZeitblockToCompareWithinGivenZeitblock(Zeitblock.ZB_19_24, zeitblock)) {
                 calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_19_24, zeitintervalle, types);
+                gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
+            }
+            if (Zeitblock.ZB_06_19.equals(zeitblock)) {
+                calculatedSpitzenstunden = calculateGleitendeSpitzenstunden(zaehlungId, Zeitblock.ZB_06_19, zeitintervalle, types);
                 gleitendeSpitzenstunden.addAll(calculatedSpitzenstunden);
             }
             if (Zeitblock.ZB_00_24.equals(zeitblock)) {
