6df7703dc4579d6e2f5cac1c73b75e0aad26b52a
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-08-31 11:13:13 +0200

    add PreAuthorize to controllers

--- src/main/java/de/muenchen/dave/controller/ConfigurationController.java
+++ src/main/java/de/muenchen/dave/controller/ConfigurationController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/configuration")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class ConfigurationController {
 
     private final ConfigurationService configurationService;
