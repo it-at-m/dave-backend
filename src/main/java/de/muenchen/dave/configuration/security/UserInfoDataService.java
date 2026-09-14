548f30115b2134a2672a24c80a7ad7bfd12a4d18
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2026-09-14 16:02:13 +0200

    fix tpyo

--- src/main/java/de/muenchen/dave/configuration/security/UserInfoDataService.java
+++ src/main/java/de/muenchen/dave/configuration/security/UserInfoDataService.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.boot.restclient.RestTemplateBuilder
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.cache.Cache;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.http.HttpEntity;
